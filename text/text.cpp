359681a8fc63f6226d9ec79b1db61cff68188885
daizxn <daizxn@outlook.com>
2023-08-12 17:06:38 +0800

    text

---
+++ text/text.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+//#include "Max.h"
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    cout<<n<<m;
+ //   cout<<MAX(n,m)<<endl;
+    cout<<"一"<<endl;
+    system("pause");
+}
